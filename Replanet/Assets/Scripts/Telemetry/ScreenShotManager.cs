690dd550554389c8ef7fb3df21729d2be55d8841
Javier Cordero <ruiderillo@hotmail.es>
2020-06-18 13:32:16 +0200

    Guardado estado de la cámara aunque con pequeño error a corregir

--- Replanet/Assets/Scripts/Telemetry/ScreenShotManager.cs
+++ Replanet/Assets/Scripts/Telemetry/ScreenShotManager.cs
@@ -1,19 +1,84 @@
 ﻿using UnityEngine;
 using System.IO;
-using Environment = System.Environment;
+using System.Collections.Generic;
+using MiniJSON;
+using Newtonsoft.Json;
 
 public class ScreenShotManager : Singleton<ScreenShotManager>
 {
-	//WARNING: Sólo funciona en Windows! 
-	private string route = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments).Replace("\\", "/") + "/My Games/Replanet/Screenshots/";
+	private string screenshotRoute;
+	private string camerainfoRoute;
+	private string camerainfoFile;
+
+	string[] fields = { "Level", "Camera_Position", "X", "Y", "Z", "Camera_Rotation", "Quaterniones" };
+
+	private void Start()
+	{
+		screenshotRoute = Application.persistentDataPath + "/Screenshots/";
+		camerainfoRoute = Application.persistentDataPath + "/Resources/";
+		camerainfoFile = "camerainfo.json";
+	}
 
 	public void TakeScreenShot()
 	{
-		if (!Directory.Exists(route))
+		if (!Directory.Exists(screenshotRoute))
 		{
-			Directory.CreateDirectory(route);
+			Directory.CreateDirectory(screenshotRoute);
 		}
 
-		ScreenCapture.CaptureScreenshot(route + "Screenshot" + ".jpg");
+		string screenshotName = "Screenshot_Level_" + Globals.actualLevel.ToString() + "TimeStamp_" +  Time.time.ToString();
+
+		ScreenCapture.CaptureScreenshot(screenshotRoute + screenshotName + ".jpg");
+		SaveCameraInfo(screenshotName);
+	}
+
+	public void SaveCameraInfo(string screenshotName)
+	{
+		//Se comprueba si el directorio donde se pretenden guardar los datos existe y si no es así se genera.
+		if (!Directory.Exists(camerainfoRoute))
+		{
+			Directory.CreateDirectory(camerainfoRoute);
+		}
+
+		//Comprobamos si el diccionario ya existe.
+		Dictionary<string, object> dict = new Dictionary<string, object>();
+		if (File.Exists(camerainfoRoute + camerainfoFile))
+		{
+			string fileText = File.ReadAllText(camerainfoRoute + camerainfoFile);
+			dict = Json.Deserialize(fileText) as Dictionary<string, object>;
+		}
+
+		//Añade las posiciones de la cámara a un diccionario para su posterior uso
+		Dictionary<string, object> CameraPositions = new Dictionary<string, object>();
+		CameraPositions.Add("X", Camera.main.transform.position.x);
+		CameraPositions.Add("Y", Camera.main.transform.position.y);
+		CameraPositions.Add("Z", Camera.main.transform.position.z);
+
+		//Añade las rotaciones de la cámara a un diccionario para su posterior uso.
+		Dictionary<string, object> CameraRatations = new Dictionary<string, object>();
+		CameraRatations.Add("X", Camera.main.transform.rotation.x);
+		CameraRatations.Add("Y", Camera.main.transform.rotation.y);
+		CameraRatations.Add("Z", Camera.main.transform.rotation.z);
+		CameraRatations.Add("W", Camera.main.transform.rotation.w);
+
+		//Un nuevo diccionario que engloba el nivel, las posiciones y las rotaciones de la cámara.
+		Dictionary<string, object> newEvent = new Dictionary<string, object>();
+		newEvent.Add("Level", Globals.actualLevel);
+		newEvent.Add("Camera_Position", CameraPositions);
+		newEvent.Add("Camera_Rotation", CameraRatations);
+
+		//Al diccionario original, añadimos el diccionario anterior dentro de uno nuevo, que será el nombre de la captura.
+		dict.Add(screenshotName, newEvent);
+
+		//Finalmente guardamos el resultado en la ruta esperada.
+		string JSONString = JsonConvert.SerializeObject(dict, Formatting.Indented);
+		File.WriteAllText(camerainfoRoute + camerainfoFile, JSONString);
+
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.Space))
+			TakeScreenShot();
 	}
 }
